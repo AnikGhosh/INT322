4ce731609c52c0b1a1379ff3af80022629ca65a4
AnikGhosh <aghosh10@myseneca.ca>
2015-02-10 14:58:23 -0500

    sDFDFDS

---
+++ INT322/public_html/index.php
@@ -0,0 +1,93 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+		<title>INT322 Winter 2015|Anik Ghosh</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+
+		<!--<style>body { padding-top: 70px; }</style>--><!-- adding padding to body to make sure nav bar doesn't overlay content-->
+
+		<!-- Bootstrap CSS -->
+		<link rel="stylesheet" href=".\bootstrap\css\bootstrap.css" media="screen">
+		<link rel="stylesheet" href=".\bootstrap\css\bootswatch.min.css">
+
+		<!-- Font Awesome CSS source from http://www.bootstrapcdn.com/#fontawesome_tab -->
+		<link href="./font-awesome.min.css" rel="stylesheet">
+
+		<!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+		<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+		<!--[if lt IE 9]>
+			<script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+			<script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+		<![endif]-->
+	</head>
+  <body>
+    <header>
+     <div class="image">
+      <img src="includes/ict-int322.png" alt="INT322 - Web Programming On Unix" title="INT322 - Web Programming On Unix" />
+     </div>
+    </header>
+    <nav>
+      <ul>
+	<li><a href="#">Assignment 1</a></li>
+	<li><a href="#">Assignment 2</a></li>
+	<li><a href="#">Assignment 3</a></li>
+	<li><a href="info.php"> Version Info </a></li>
+	 <li><a href="sample.php"> Sample file </a></li>
+      </ul>
+    </nav>
+    <div id="content">
+      <div id="mcontent">
+	<!-- Defining Header for content section -->
+	<header>
+	  <h2>Ghosh Anik</h2>
+	</header>
+	<!-- Defining Content Section -->
+	<section>
+	  <!-- Defining content section article -->
+	  <article class="bpage">
+	    <div class="image">
+	      <canvas id="myCanvas"></canvas>
+	    </div>
+	  </article>
+	</section>
+      </div>
+      <!-- Defining Sidebar / Aside -->
+      <aside>
+	<section>
+	 <ul>
+	   <li><a href="http://www.senecac.on.ca/">Seneca College</a></li>
+	   <li><a href="https://scs.senecac.on.ca/">School of ICT</a></li>
+	   <li><a href="https://scs.senecac.on.ca/course/int322">INT322 Course Outline</a></li>
+	   <li><a href="https://my.senecacollege.ca/webapps/portal/frameset.jsp">My Seneca</a></li>
+	 </ul>
+	</section>
+	<section>
+	  <ul>
+	    <li><a href="http://www.senecac.on.ca/policies/itau.html">Seneca College Acceptable Use Policy</a></li>  
+	    <li><a href="https://scs.senecac.on.ca/page/acadamic-policy-cheatings">Academic Policy / Cheating</a></li>
+	  </ul>
+	</section>
+	<section>
+	  <ul>
+	    <li><a href="practice/">Exercises / Labs</a></li> 
+	    <!-- Replace the ??? with your learn user name -->
+	    <li><address><a href="mailto:aghosh10@myseneca.ca?subject=INT322">Your learn email</a></address></li>
+	  </ul>
+	</section>
+      </aside>
+    </div>
+    <!-- Defining Footer -->
+    <footer>
+      <section>
+	<script type="text/javascript">
+	//<![CDATA[
+	  var dt=new Date(document.lastModified);   // Get document last modified date
+	  document.write('This page was last updated on '+dt.toLocaleString()) 
+	//]]>
+	</script>
+      </section>
+    </footer>
+  </body>
+</html>
---
+++ INT322/public_html/test.php
@@ -0,0 +1,87 @@
+<!DOCTYPE html>
+<html lang="">
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<title>INT322 Winter 2015|Anik Ghosh</title>
+
+		<style>body { padding-top: 70px; }</style><!-- adding padding to body to make sure nav bar doesn't overlay content-->
+}
+
+		<!-- Bootstrap CSS -->
+		<link rel="stylesheet" href="./bootstrap.css" media="screen">
+		<link rel="stylesheet" href="./bootswatch.min.css">
+
+		<!-- Font Awesome CSS source from http://www.bootstrapcdn.com/#fontawesome_tab -->
+		<link href="./font-awesome.min.css" rel="stylesheet">
+
+		<!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+		<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+		<!--[if lt IE 9]>
+			<script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+			<script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+		<![endif]-->
+	</head>
+	<body>
+	   <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+		<div class="container-fluid">
+
+			<!-- Brand and toggle get grouped for better mobile display -->
+			<div class="navbar-header">
+			  <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+				<span class="sr-only">Toggle navigation</span>
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+			 </button>
+			 <a class="navbar-brand" href="#">Sales Broadcast Tools</a>
+			</div>
+	
+		  <!-- Collect the nav links, forms, and other content for toggling -->
+		   <div class="collapse navbar-collapse navbar-ex1-collapse">
+			  <ul class="nav navbar-nav ">
+				<li class="active"><a href="#">Select Recipients</a></li>
+			  </ul>
+			  <ul class="nav navbar-nav">
+				<li><a href="#">Compose E-mail</a></li>
+			  </ul>
+			  <ul class="nav navbar-nav navbar-right">
+				<li class="dropdown">
+				  <a href="#" class="dropdown-toggle" data-toggle="dropdown">Add/Edit Contact <span class="caret"></span></a>
+				        <ul class="dropdown-menu">
+						<li><a href="#">Add a contact</a></li>
+						<li><a href="#">Edit a contact</a></li>					
+					   </ul>
+				</li>
+			  </ul>
+		   </div><!-- /.navbar-collapse -->
+		</div><!-- /.container-fluid-->
+        </nav>
+
+			<!--brand logo-->
+		<div align="center">
+			<img id="ctl00_Image1" src="ConstantContactLogo.jpg" style="border-width:0px;" />
+			<br />
+			<br />
+		</div>
+
+		 <nav class="navbar navbar-default navbar-fixed-bottom">
+			<div class="container-fluid">
+			<footer class="footer">
+			   <div class="container"><p class="text-muted">Place sticky footer content here.</p></div>
+			</footer>
+			</div>
+		</nav>
+
+
+		<!-- Bootstrap core JavaScript -->
+   		<!--================================================== -->
+    		<!-- Placed at the end of the document so the pages load faster -->
+    		<script src="./jquery.min.js"></script>
+    		<script src="./bootstrap.min.js"></script>
+    		<!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+    		<script src="./ie10-viewport-bug-workaround.js"></script>
+    		<script src="./bootswatch.js"></script>
+  	</body>
+  </html>
