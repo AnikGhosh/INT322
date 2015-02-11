a737540a700cb258d3cf6d2939ba598aec1cbb8c
AnikGhosh <aghosh10@myseneca.ca>
2015-02-10 20:52:09 -0500

    updated layout

--- test.php
+++ test.php
@@ -5,16 +5,16 @@
 		<meta http-equiv="X-UA-Compatible" content="IE=edge">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 		<title>INT322 Winter 2015|Anik Ghosh</title>
+ 
+        <link rel="stylesheet" href="includes/reset.css"   type="text/css" media="screen"> 
 
 		<style>body { padding-top: 70px; }</style><!-- adding padding to body to make sure nav bar doesn't overlay content-->
-}
 
 		<!-- Bootstrap CSS -->
-		<link rel="stylesheet" href="./bootstrap.css" media="screen">
-		<link rel="stylesheet" href="./bootswatch.min.css">
+		<link rel="stylesheet" href=".\bootstrap\css\bootstrap.css" media="screen">
 
 		<!-- Font Awesome CSS source from http://www.bootstrapcdn.com/#fontawesome_tab -->
-		<link href="./font-awesome.min.css" rel="stylesheet">
+		<!--<link href=".\bootstrap\font-awesome.min.css" rel="stylesheet">-->
 
 		<!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
 		<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
@@ -24,53 +24,68 @@
 		<![endif]-->
 	</head>
 	<body>
-	   <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
-		<div class="container-fluid">
 
-			<!-- Brand and toggle get grouped for better mobile display -->
-			<div class="navbar-header">
-			  <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
-				<span class="sr-only">Toggle navigation</span>
-				<span class="icon-bar"></span>
-				<span class="icon-bar"></span>
-				<span class="icon-bar"></span>
-			 </button>
-			 <a class="navbar-brand" href="#">Sales Broadcast Tools</a>
-			</div>
-	
-		  <!-- Collect the nav links, forms, and other content for toggling -->
-		   <div class="collapse navbar-collapse navbar-ex1-collapse">
-			  <ul class="nav navbar-nav ">
-				<li class="active"><a href="#">Select Recipients</a></li>
-			  </ul>
-			  <ul class="nav navbar-nav">
-				<li><a href="#">Compose E-mail</a></li>
-			  </ul>
-			  <ul class="nav navbar-nav navbar-right">
-				<li class="dropdown">
-				  <a href="#" class="dropdown-toggle" data-toggle="dropdown">Add/Edit Contact <span class="caret"></span></a>
-				        <ul class="dropdown-menu">
-						<li><a href="#">Add a contact</a></li>
-						<li><a href="#">Edit a contact</a></li>					
-					   </ul>
-				</li>
-			  </ul>
-		   </div><!-- /.navbar-collapse -->
-		</div><!-- /.container-fluid-->
-        </nav>
+        <!--brand logo-->
+		    <header>
+     		  <div class="image">
+      			<img id="ctl00_Image1" src="includes/ict-int322.png" alt="INT322 - Web Programming On Unix" title="INT322 - Web Programming On Unix" /><!-- style="border-width:0px;" />-->
+     		  </div>
+    	    </header><br /><br />
 
-			<!--brand logo-->
-		<div align="center">
-			<img id="ctl00_Image1" src="ConstantContactLogo.jpg" style="border-width:0px;" />
-			<br />
-			<br />
-		</div>
+        <!--INVERSE NAVBAR-->
+
+	  <nav class="navbar navbar-inverse">
+        <div class="container-fluid">
+           <div class="navbar-header">
+              <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-2">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+              </button>
+              <a class="navbar-brand" href="#">Brand</a>
+            </div>
+
+        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-2">
+          <ul class="nav navbar-nav">
+            <li class="active"><a href="#">Link <span class="sr-only">(current)</span></a></li>
+            <li><a href="#">Link</a></li>
+            <li class="dropdown">
+               <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Dropdown <span class="caret"></span></a>
+               <ul class="dropdown-menu" role="menu">
+                <li><a href="#">Action</a></li>
+                <li><a href="#">Another action</a></li>
+                <li><a href="#">Something else here</a></li>
+                <li class="divider"></li>
+                <li><a href="#">Separated link</a></li>
+                <li class="divider"></li>
+                <li><a href="#">One more separated link</a></li>
+               </ul>
+            </li>
+          </ul>
+      
+            <ul class="nav navbar-nav navbar-right">
+                <li><a href="#">Link</a></li>
+            </ul>
+         </div><!--navbar-collapse ends-->
+        </div> <!--container fluid class ends-->
+      </nav>
+
+        <!-- INVERSE NAVBAR ENDS -->			
 
 		 <nav class="navbar navbar-default navbar-fixed-bottom">
 			<div class="container-fluid">
-			<footer class="footer">
-			   <div class="container"><p class="text-muted">Place sticky footer content here.</p></div>
-			</footer>
+			 <footer class="footer">
+			   <div class="container">
+                   <section class="text-muted">
+	                 <script type="text/javascript">
+	                  //<![CDATA[
+	                     var dt=new Date(document.lastModified);   // Get document last modified date
+	                     document.write('This page was last updated on '+dt.toLocaleString()) 
+	                  //]]>
+	                 </script>
+                   </section>
+			   </div>
+			  </footer>
 			</div>
 		</nav>
 
@@ -78,10 +93,11 @@
 		<!-- Bootstrap core JavaScript -->
    		<!--================================================== -->
     		<!-- Placed at the end of the document so the pages load faster -->
-    		<script src="./jquery.min.js"></script>
-    		<script src="./bootstrap.min.js"></script>
+            <!----><script src="includes/myscript.js"  type="text/javascript"></script> 
+    		<script src=".\bootstrap\js\jquery.min.js"></script>
+    		<script src=".\bootstrap\js\bootstrap.js"></script>
     		<!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
-    		<script src="./ie10-viewport-bug-workaround.js"></script>
-    		<script src="./bootswatch.js"></script>
-  	</body>
-  </html>
+    		<script src=".\bootstrap\js\ie10-viewport-bug-workaround.js"></script>
+    		<script src=".\bootstrap\js\bootswatch.js"></script>
+  </body>
+</html>
